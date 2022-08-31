0a6f9b4f0d133835c1f60198dab695dbcd50c764
Abdul Rafique <contact2rafique@gmail.com>
2022-08-31 14:16:56 +0500

    configured tailwindcss

---
+++ tailwind.config.cjs
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    colors: {
+      primary: {
+        light: "#adb8f6",
+        DEFAULT: "#a4b0f5",
+        dark: "#949edd",
+      },
+      secondary: {
+        light: "#dd8125",
+        DEFAULT: "#f58f29",
+        dark: "#dd8125",
+      },
+      accent: {
+        light: "#5774b5",
+        DEFAULT: "#4464ad",
+        dark: "#3d5a9c",
+      },
+      white: "#fff",
+      transparent: colors.transparent,
+    },
+  },
+  plugins: [],
+};
